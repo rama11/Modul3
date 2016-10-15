b010b8fa541c1a1451a74ca527eeac6b7c32cb2e
Rama <prof.agastyo@gmail.com>
2016-10-15 23:31:51 +0700

    + Dialog

--- app/src/main/java/com/nbs/app/modul3/dialogs.java
+++ app/src/main/java/com/nbs/app/modul3/dialogs.java
@@ -1,14 +1,47 @@
 package com.nbs.app.modul3;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class dialogs extends AppCompatActivity {
+
+    private Button btnDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dialogs);
         getSupportActionBar().setTitle("Dialogs");
+
+        btnDialog = (Button) findViewById(R.id.btnDialog);
+
+        btnDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(dialogs.this, R.style.AlertDialogCustom);
+                builder.setTitle("Sample Alert");
+                builder.setMessage("Apakah anda yakin akan keluar?");
+                builder.setPositiveButton("Yes",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                moveTaskToBack(true);
+                                android.os.Process.killProcess(android.os.Process.myPid());
+                                System.exit(1);
+                            }
+                        });
+                builder.setNegativeButton("No",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(getApplicationContext(), "No, you not exit", Toast.LENGTH_LONG).show();
+                            }
+                        });
+                builder.show();
+            }
+        });
     }
 }
